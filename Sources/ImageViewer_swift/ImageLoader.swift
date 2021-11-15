88a5d62fb5f5d3893ffd4951362af3d39cff31a0
Karo Sahafi <karosahafi@gmail.com>
2021-11-16 00:58:18 +0330

    Remove URL Session Image Loader

--- Sources/ImageViewer_swift/ImageLoader.swift
+++ Sources/ImageViewer_swift/ImageLoader.swift
@@ -4,24 +4,3 @@ import UIKit
 public protocol ImageLoader {
     func loadImage(_ url: URL, placeholder: UIImage?, imageView: UIImageView, completion: @escaping (_ image: UIImage?) -> Void)
 }
-
-public struct URLSessionImageLoader: ImageLoader {
-    public init() {}
-
-    public func loadImage(_ url: URL, placeholder: UIImage?, imageView: UIImageView, completion: @escaping (UIImage?) -> Void) {
-        if let placeholder = placeholder {
-            imageView.image = placeholder
-        }
-
-        DispatchQueue.global(qos: .background).async {
-            guard let data = try? Data(contentsOf: url), let image = UIImage(data: data) else {
-                completion(nil)
-                return
-            }
-
-            DispatchQueue.main.async {
-                imageView.image = image
-            }
-        }
-    }
-}
