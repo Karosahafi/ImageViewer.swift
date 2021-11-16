cf7b33c736aa666323a4e5a4b7c154d797e9f9cd
Karo Sahafi <karosahafi@gmail.com>
2021-11-16 20:17:51 +0330

    Change animation time

--- Sources/ImageViewer_swift/ImageViewerTransitionDismiss.swift
+++ Sources/ImageViewer_swift/ImageViewerTransitionDismiss.swift
@@ -10,7 +10,7 @@ import UIKit
 
 final class ImageViewerTransitionDismiss: NSObject, UIViewControllerAnimatedTransitioning, DummyImageCreator {
     func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
-        3
+        0.320
     }
 
     func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
--- Sources/ImageViewer_swift/ImageViewerTransitionPresenting.swift
+++ Sources/ImageViewer_swift/ImageViewerTransitionPresenting.swift
@@ -10,7 +10,7 @@ import UIKit
 
 final class ImageViewerTransitionPresenting: NSObject, UIViewControllerAnimatedTransitioning, DummyImageCreator {
     func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
-        3
+        0.320
     }
 
     func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
